1fbb9d27efbdf7ea0ab420d31fd67cb61d6ebf1b
Ayush <natikagarwal456@gmail.com>
2026-06-05 09:32:41 +0530

    Fix firebase-admin import syntax in send-email API and update vurlo99.zip

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { Resend } from "resend";
-import * as admin from "firebase-admin";
+import admin from "firebase-admin";
 import { IncomingForm } from "formidable";
 import fs from "fs";
 
